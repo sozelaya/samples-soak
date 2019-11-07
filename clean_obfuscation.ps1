d1c1ec7fb12943bde802e74f51149278f906cd76
sozelaya <51933757+sozelaya@users.noreply.github.com>
2019-11-07 17:34:42 -0300

    Add files via upload

---
+++ clean_obfuscation.ps1
@@ -0,0 +1,3 @@
+$base64 = "FHJ+YHoTZ1ZARxNgUl5DX1YJEwRWBAFQAFBWHgsFAlEeBwAACh4LBAcDHgNSUAIHCwdQAgALBRQ="
+$bytes = [Convert]::FromBase64String($base64)
+$string = -join ($bytes | % { [char] ($_ -bxor 0x33) })
---
+++ reflective_popup.ps1
@@ -0,0 +1,2 @@
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") >$null 2>&1
+[System.Windows.Forms.Messagebox]::Show("Hello QA!", "Message from ASDC") >$null 2>&1
