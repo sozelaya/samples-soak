4a85451902ffb106edd31a46d46fae2dede2c22d
sozelaya <51933757+sozelaya@users.noreply.github.com>
2021-01-26 15:05:34 -0300

    Delete get_system_uptime.ps1

--- get_system_uptime.ps1
+++
@@ -1,8 +0,0 @@
-function Get-SystemUptime ($computer = "$env:computername") {
-  $lastboot = [System.Management.ManagementDateTimeconverter]::ToDateTime("$((gwmi  Win32_OperatingSystem).LastBootUpTime)")
-  $uptime = (Get-Date) - $lastboot
-  #Write-Host "System Uptime for $computer is: " $uptime.days "days" $uptime.hours "hours" $uptime.minutes "minutes" $uptime.seconds "seconds"
-  return (($uptime.days).ToString()+"d:"+($uptime.hours).ToString()+"h:"+$uptime.minutes.ToString()+"m:"+($uptime.seconds).ToString()+"s")
-}
-
-Get-SystemUptime
